3b1417100b6ff9ef757c98ad4c934eb9575f7a27
WeeD-Indica <53754913+WeeD-Indica@users.noreply.github.com>
2019-10-31 12:24:15 +0530

    31/10

--- tree.c
+++ tree.c
@@ -72,7 +72,7 @@ int traversal(int c,node* tptr)
     {
       if(tptr==NULL) return 0;
       else{
-	printf("%d",tptr->data);
+	printf("%d ",tptr->data);
 	traversal(1,tptr->left);
 	traversal(1,tptr->right);
 	return  0;
@@ -80,23 +80,23 @@ int traversal(int c,node* tptr)
     }
   if (c==2)     //LNR
     {
-     if(tptr==NULL) return 0;
-      else
-      {
-	    traversal(2,tptr->left);
-	    printf("%d",tptr->data);
+      if(tptr==NULL) return 0;
+     else
+	{
+	traversal(2,tptr->left);
+	    printf("%d ",tptr->data);
 	    traversal(2,tptr->right);
-        return 0;  
+	    return 0;  
       }
     }
   if (c==3)     //LRN
     {
       if(tptr==NULL) return 0;
       else
-      {
-          traversal(3,tptr->left);
-	      traversal(3,tptr->right);
-	      printf("%d",tptr->data);
+	{
+	traversal(3,tptr->left);
+	  traversal(3,tptr->right);
+	      printf("%d ",tptr->data);
 	      return 0;
       }
     }
@@ -105,64 +105,75 @@ int traversal(int c,node* tptr)
   return 0;
 }
 
-void deletion(node* tptr)
+void deletion(node *tptr)
 {
-    node* cur=t,*prev=NULL;
-    while(cur!=tptr)
+  node *ptr,*parent=NULL,*s=NULL;
+  int temp;
+  if(t==NULL)
+    printf("Empty Tree ");
+  else
     {
-        prev=cur;
-        if(tptr==cur) break;
-        else if(tptr->data>cur->data) cur=cur->right;
-        else if(tptr->data<cur->data) cur=cur->left;
-    }
-
-    if(tptr->right==NULL && tptr->left==NULL)
+      ptr=t;
+      int flag=0;
+      while((ptr!=NULL)&&(flag==0))
+	{
+	  if(tptr->data>ptr->data)
+	    {
+	      parent=ptr;
+	      ptr=ptr->right;
+	    }
+	  else if(tptr->data<ptr->data)
+	    {
+	      parent=ptr;
+	      ptr=ptr->left;
+	    }
+	  else
+      {
+	flag=1;
+      }
+	}
+      if(flag==0)
+	printf("Value Not found");
+      else if((ptr->left==NULL)&&(ptr->right==NULL))
+	{
+	  if(parent->left==ptr)
+	    parent->left=NULL;
+	  else
+	    parent->right=NULL;
+	  free(ptr);
+	}
+      else if((ptr->left!=NULL)&&(ptr->right!=NULL))
     {
-        if(prev->right==tptr){
-            free(tptr);
-            prev->right=NULL;
-        }
-        else prev->left=NULL;
+      s=ptr->right;
+      while(s->left!=NULL)
+	{ 
+	  s=s->left;
+	}
+      temp=s->data; 
+    
+    deletion(s); 
+    ptr->data=temp;
+    //free(ptr);
     }
-    else if(tptr->right!=NULL && tptr->left!=NULL)
-    {
-        node* cur=tptr->right,*prev;
-        while(cur->left!=NULL)
-        {
-            prev=cur;
-            cur=cur->left;
-        }
-        tptr->data=cur->data;
-        free(cur);
-    }
-    else
-    {
-        if(tptr==prev->right)
-        {
-            if(tptr->right!=NULL) 
-            {
-                prev->right=tptr->right;
-                free(tptr);
-            }
-            else
-            {
-                prev->right=tptr->left;
-                free(tptr);
-            }
-        }
-        else{
-            if(tptr->right!=NULL){
-                prev->left=tptr->right;
-                free(tptr);
-            }
-            else{
-                prev->left=tptr->left;
-                free(tptr);
-            }
-        }
-    }
-
-
+      else
+	{
+	  if(parent->left==ptr)
+	    {
+	      if(ptr->left==NULL)
+		parent->left=ptr->right;
+	      else
+		parent->left=ptr->left;
+	    } 
+	  else if(parent->right==ptr)
+	    {
+	      if(ptr->left==NULL)
+		parent->right=ptr->right;
+	      else
+		parent->right=ptr->left;
+	    }  
+	  free(ptr);   
+	}    
+    }      
 }
 
 node* search(int x)
